b28c24a03e5992e1374f27ddd66119914d8f0c4c
davidrbgb <125749736+davidrbgb@users.noreply.github.com>
2023-02-20 22:09:53 +0000

    Atribuir porto dinamicamente
    Atribuir porto dinamicamente

--- Projeto/code/server/app.js
+++ Projeto/code/server/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000")
-})
+const port = process.env.PORT || 3000;
+app.listen(port,() => {
+    console.log(`Listening on port ${port}!`);
+});
