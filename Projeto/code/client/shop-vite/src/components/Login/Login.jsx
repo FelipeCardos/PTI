8bdd749b6f5efaff7c862ef5e9a2123a39524ff4
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-02-27 18:16:39 +0000

    Componente Login criada

---
+++ Projeto/code/client/shop-vite/src/components/Login/Login.jsx
@@ -0,0 +1,44 @@
+export default function Login(props) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // submitToApi(formData)
+    console.log(formData);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='E-mail'
+          onChange={handleChange}
+          name='email'
+          value={formData.email}
+        />
+        <input
+          type='password'
+          placeholder='Palavra-passe'
+          onChange={handleChange}
+          name='password'
+          value={formData.password}
+        />
+        <button type='submit'>Entrar</button>
+      </form>
+    </div>
+  );
+}
