7b99c6c9dbfdc78b8cfb1bad1753f1818643a8c9
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-03-09 12:09:07 +0000

    Página não dá load, google logIn ainda não implementado

--- Projeto/code/client/shop-vite/src/components/Login/Login.jsx
+++ Projeto/code/client/shop-vite/src/components/Login/Login.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { GoogleLogin } from "@react-oauth/google";
 import "./Login.css";
 
 export default function Login(props) {
@@ -76,14 +74,6 @@ export default function Login(props) {
         </div>
         <div>
           <input type='submit' className='loginButton' value='SIGN IN' />
-          {/* <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          /> */}
         </div>
         <hr className='hr' />
         <div>
--- Projeto/code/client/shop-vite/src/main.jsx
+++ Projeto/code/client/shop-vite/src/main.jsx
@@ -1,4 +1,3 @@
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -26,6 +25,4 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-  // <GoogleOAuthProvider clientId='605017327713-opokg48ftsd42qfmg8pqonroui009qrn.apps.googleusercontent.com'>
-  // </GoogleOAuthProvider>
 );
