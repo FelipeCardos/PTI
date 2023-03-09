f0dab70a35b3154b30b72272da6c3deaf187c157
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-03-09 10:27:32 +0000

    Wrap da aplicação pelo GoogleOAuthProvider

--- Projeto/code/client/shop-vite/src/main.jsx
+++ Projeto/code/client/shop-vite/src/main.jsx
@@ -1,3 +1,4 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -24,5 +25,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <GoogleOAuthProvider clientId='605017327713-opokg48ftsd42qfmg8pqonroui009qrn.apps.googleusercontent.com'>
+    <RouterProvider router={router} />
+  </GoogleOAuthProvider>
 );
