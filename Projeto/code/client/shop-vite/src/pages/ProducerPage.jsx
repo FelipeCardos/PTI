0c0d3487f30b08232db0632de71f68a96f9559c8
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-03-20 11:04:49 +0000

    ProducerPage redefinida, quando vejo um fornecedor é esta a página que eu quero

--- Projeto/code/client/shop-vite/src/pages/ProducerPage.jsx
+++ Projeto/code/client/shop-vite/src/pages/ProducerPage.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 
 export default function ProducerPage() {
-  let client = true; // If the logged in user is a client or a producer (teste)
-  const { id } = useParams(); // Gets the id of the producer
+  const { id } = useParams(); // Gets the id of the producer from the url
   return (
     <div id='producer-page'>
-      {/* If its a client, shows the name of the producer and their products
-      If its a producer, shows the name of the producer, options like the local report, their sales, what products have been sold the most, how much revenue they had the past week/month, theirProductionUnits and its products */}
-      {client} && (<div>Producers name</div>
-      <div>Producers products</div>){!client} && (<div>Producers name</div>
-      <div>Producers options</div>
-      <div>Producers sales</div>
-      <div>Producers products</div>
-      <div>Producers ProductionUnits</div>
-      <div>Producers ProductionUnits products</div>
-      <div>Producers ProductionUnits products</div>
-      <div>Producers ProductionUnits products</div>
-      <div>Producers ProductionUnits products</div>)
+      {/* If its a producer, shows the name of the producer, options like the local report, their sales, what products have been sold the most, how much revenue they had the past week/month, theirProductionUnits and its products */}
+      <div id='producer-info'>
+        <div>Producer's Name</div>
+        <div>Production Unit</div>
+        <div>Products</div>
+      </div>
     </div>
   );
 }
