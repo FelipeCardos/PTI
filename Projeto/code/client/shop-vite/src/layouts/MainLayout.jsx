c42bd2364accfee0af9802ffb4d816e81845ba2a
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-04-14 13:36:55 +0100

    Cor principal é o azul #0d6efd

--- Projeto/code/client/shop-vite/src/layouts/MainLayout.jsx
+++ Projeto/code/client/shop-vite/src/layouts/MainLayout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Footer from "../components/Footer/footer";
-import Header from "../components/Header/header";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 export default function MainLayout({ children }) {
   return (
     <>
       <Header></Header>
-      <hr />
       <div style={{ display: "inline-block", position: "relative" }}>
         {children}
       </div>
