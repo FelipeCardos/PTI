d2278c4c713cddc33c9450bf1d7b962013655826
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-04-16 15:00:10 +0100

    Secção das notificações concluída??

--- Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProducerManagementArea.jsx
+++ Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProducerManagementArea.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Notifications from "./Notifications/Notifications";
 import "./ProducerManagementArea.css";
 
 export default function ProducerManagementArea() {
   return (
     <div className='containerProducerManagementArea'>
-      <div className='containerNotifications'></div>
+      <div className='containerNotifications'>
+        <Notifications />
+      </div>
       <div className='containerProductionUnits'></div>
       <div className='containerVehicles'></div>
     </div>
