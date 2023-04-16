f3db1a38910f124390e927bbeb4fd149bb32a046
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-04-16 16:25:48 +0100

    Criação dos forms de add de unidades de produção e veículos

--- Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProducerManagementArea.jsx
+++ Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProducerManagementArea.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { React, useState } from "react";
 import Notifications from "./Notifications/Notifications";
 import "./ProducerManagementArea.css";
 import ProductionUnits from "./ProductionUnits/ProductionUnits";
 import Vehicles from "./Vehicles/Vehicles";
 
 export default function ProducerManagementArea() {
+  const [modal, setModal] = useState(false);
   return (
     <div className='containerProducerManagementArea'>
       <div className='containerNotifications'>
@@ -16,6 +17,7 @@ export default function ProducerManagementArea() {
       <div className='containerVehicles'>
         <Vehicles />
       </div>
+      {modal && <div className='modalProducerManagementArea'></div>}
     </div>
   );
 }
