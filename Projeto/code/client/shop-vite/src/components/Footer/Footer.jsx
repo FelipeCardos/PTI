3337164b9b65ed2663ff75fc1d606420d044513d
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-04-16 14:11:22 +0100

    Começo da criação da página de gestão de unidades de produção, de veículos e de notificações do fornecedor autenticado

--- Projeto/code/client/shop-vite/src/components/Footer/Footer.jsx
+++ Projeto/code/client/shop-vite/src/components/Footer/Footer.jsx
@@ -4,28 +4,26 @@ export default function Footer(props) {
   return (
     <div className='footer-clean'>
       <footer>
-        <div className='container'>
-          <div className='row justify-content-center'>
-            <div className='col-sm-4 col-md-3 item'>
-              <h3>About</h3>
-              <ul>
-                <li>
-                  <a href='#'>Company</a>
-                </li>
-                <li>
-                  <a href='#'>Team</a>
-                </li>
-                <li>
-                  <a href='#'>Legacy</a>
-                </li>
-              </ul>
-            </div>
-            <div className='col-lg-3 item social'>
-              <a href='#'>
-                <i className='fab fa-github' />
-              </a>
-              <p className='copyright'>LocalShop © 2023</p>
-            </div>
+        <div className='row justify-content-center'>
+          <div className='col-sm-4 col-md-3 item'>
+            <h3>About</h3>
+            <ul>
+              <li>
+                <a href='#'>Company</a>
+              </li>
+              <li>
+                <a href='#'>Team</a>
+              </li>
+              <li>
+                <a href='#'>Legacy</a>
+              </li>
+            </ul>
+          </div>
+          <div className='col-lg-3 item social'>
+            <a href='#'>
+              <i className='fab fa-github' />
+            </a>
+            <p className='copyright'>LocalShop © 2023</p>
           </div>
         </div>
       </footer>
---
+++ Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProducerManagementArea.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProducerManagementArea.css";
+
+export default function ProducerManagementArea() {
+  return (
+    <div className='containerProducerManagementArea'>
+      <div className='containerNotifications'></div>
+      <div className='containerProductionUnits'></div>
+      <div className='containerVehicles'></div>
+    </div>
+  );
+}
