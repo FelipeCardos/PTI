852db7d40142a134fbdb68bd748551d773d9ca1e
joaoglmartins <93201286+joaoglmartins@users.noreply.github.com>
2023-04-17 18:16:29 +0100

    Primeiro teste de sign in com o google

--- Projeto/code/client/shop-vite/src/components/Signin/Signin.jsx
+++ Projeto/code/client/shop-vite/src/components/Signin/Signin.jsx
@@ -47,10 +47,13 @@ export default function Signin(props) {
       });
   }
 
-  function handleGoogleSignin(event) {
-    window.location.href =
-      "http://localhost:3000/api/v1/auth/google/signin?typeUser=" +
-      props.typeUser;
+  function handleGoogleSignin(typeUser) {
+    if (typeUser === "Consumer") {
+      window.location.href = "http://localhost:3000/api/v1/auth/google/login";
+    } else if (typeUser === "Producer") {
+      window.location.href =
+        "http://localhost:3000/api/v1/auth/google/login?isProducer=true";
+    }
   }
 
   return (
@@ -92,16 +95,54 @@ export default function Signin(props) {
           <div>
             <input type='submit' className='signinButton' value='SIGN IN' />
           </div>
-          <div className='google-btn' onClick={handleGoogleSignin}>
-            <div className='google-icon-wrapper'>
-              <img
-                className='google-icon'
-                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-              />
+          <div className='signinGoogleButtons'>
+            <div
+              className='signinGoogleButton'
+              onClick={() => {
+                handleGoogleSignin("Consumer");
+              }}
+            >
+              <svg
+                style={{ color: "white" }}
+                xmlns='http://www.w3.org/2000/svg'
+                width={16}
+                height={16}
+                fill='currentColor'
+                className='bi bi-google'
+                viewBox='0 0 16 16'
+              >
+                {" "}
+                <path
+                  d='M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z'
+                  fill='white'
+                />
+              </svg>
+
+              <span>CONSUMER</span>
             </div>
-            <p className='btn-text'>
-              <b>Sign in with google</b>
-            </p>
+            <div
+              className='signinGoogleButton'
+              onClick={() => {
+                handleGoogleSignin("Producer");
+              }}
+            >
+              <svg
+                style={{ color: "white" }}
+                xmlns='http://www.w3.org/2000/svg'
+                width={16}
+                height={16}
+                fill='currentColor'
+                className='bi bi-google'
+                viewBox='0 0 16 16'
+              >
+                {" "}
+                <path
+                  d='M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z'
+                  fill='white'
+                />
+              </svg>
+              <span>PRODUCER</span>
+            </div>
           </div>
 
           <hr className='hr' />
