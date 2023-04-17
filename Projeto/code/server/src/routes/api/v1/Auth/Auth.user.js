2f6238eb98028dcd4eac60d5c0ce346b70e303bc
bernardo <77997419+bernardo-rebelo@users.noreply.github.com>
2023-04-17 20:39:23 +0100

    auth user

---
+++ Projeto/code/server/src/routes/api/v1/Auth/Auth.user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { checkAuthenticated } = require('../../../../middleware/UserAuth');
+
+// import routes
+router.get('/', checkAuthenticated ,(req, res) => {
+    res.status(200).json({ userID: req.user.id });
+});
+
+
+
+module.exports = router;
--- Projeto/code/server/src/routes/api/v1/User/payment.js
+++ Projeto/code/server/src/routes/api/v1/User/payment.js
@@ -4,22 +4,24 @@ const {
   checkAuthenticated,
   checkUsersIsProducer,
   checkUsersIsConsumer,
+  checkIfUserIsOwnerOfTheResource,
 } = require("../../../../middleware/UserAuth");
 const { GetCartCost } = require("../../../../controllers/Cart/getCartCost");
 
 const router = express.Router();
 // passport.authenticate("jwt", { session: false })
 
-router.get("/", checkAuthenticated, (req, res) => {
+router.get("/", (req, res) => {
+  req.user;
   // GetCartCost(1).then((cartCost) => {
   //     console.log(cartCost);
   //     res.status(200).json({cartCost: cartCost});
   // });
-  let user = req.user;
-  console.log(
-    "-------------------------------------------------------------------"
-  );
-  console.log(user);
+  // let user = req.user;
+  // console.log(
+  //   "-------------------------------------------------------------------"
+  // );
+  // console.log(user);
   res.send("Hello World");
 });
 
