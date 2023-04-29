870eb1dca1e554d4608068083b86ec7456fc2f12
bernardo <77997419+bernardo-rebelo@users.noreply.github.com>
2023-04-29 18:42:21 +0100

    just server

--- Projeto/code/client/shop-vite/src/components/Producer/ProductionUnit.jsx
+++
@@ -1,8 +0,0 @@
-export default function ProductionUnit(props) {
-  return (
-    <div className='productionUnit'>
-      <div>{props.productionUnitName}</div>
-      <div>{props.productionUnitLocation}</div>
-    </div>
-  );
-}
--- Projeto/code/client/shop-vite/src/components/ProducerManagementArea/Notifications/NotificationExample/NotificationExample.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./NotificationExample.css";
-
-export default function NotificationExample(props) {
-  return <div className='notificationExample'>{props.description}</div>;
-}
