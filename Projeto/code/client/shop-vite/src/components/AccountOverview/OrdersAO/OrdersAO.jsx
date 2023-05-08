bbaa8884264fdf691b6057184e998fda4c905849
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-08 15:52:10 +0100

    Inicio da secção das Encomendas do utilizador

--- Projeto/code/client/shop-vite/src/components/AccountOverview/OrdersAO/OrdersAO.jsx
+++ Projeto/code/client/shop-vite/src/components/AccountOverview/OrdersAO/OrdersAO.jsx
@@ -1,3 +1,18 @@
 import { React, useEffect, useState } from "react";
+import "./OrdersAO.css";
 
-export default function OrdersAO(props) {}
+export default function OrdersAO(props) {
+  return (
+    <div className='containerOrdersAOHeader'>
+      <div className='containerOrdersAOHeaderTitle'>Orders</div>
+      <div className='containerOrdersAOHeaderButtons'>
+        <button className='containerOrdersAOHeaderLocalImpactReport'>
+          View Local Impact Report
+        </button>
+        <button className='containerOrdersAOHeaderExportJSON'>
+          Export to JSON
+        </button>
+      </div>
+    </div>
+  );
+}
