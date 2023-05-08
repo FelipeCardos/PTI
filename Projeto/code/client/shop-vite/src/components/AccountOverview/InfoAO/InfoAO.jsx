3475fa91646f73e33c8727b40447ca36788c9c78
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-08 13:10:06 +0100

    Penso que a secção de account information já está boa

--- Projeto/code/client/shop-vite/src/components/AccountOverview/InfoAO/InfoAO.jsx
+++ Projeto/code/client/shop-vite/src/components/AccountOverview/InfoAO/InfoAO.jsx
@@ -1,19 +1,31 @@
 import axios from "axios";
 import { React, useEffect, useState } from "react";
 import "./InfoAO.css";
-import InfoAOFields from "./InfoAOFields/InfoAOFields";
 
 export default function InfoAO(props) {
   const [editMode, setEditMode] = useState(false);
   const [changePassword, setChangePassword] = useState(false);
   const [deleteAccount, setDeleteAccount] = useState(false);
-  const [deleteAccountPassword, setDeleteAccountPassword] = useState(false);
+  const [deleteAccountPassword, setDeleteAccountPassword] = useState("");
+  const [deleteAccountPasswordCorrect, setDeleteAccountPasswordCorrect] =
+    useState(false);
+  const [formDataAccount, setFormDataAccount] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    fiscal_identifier: "",
+    address: "",
+  });
+  const [formDataPassword, setFormDataPassword] = useState({
+    currentPassword: "",
+    newPassword: "",
+  });
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       console.log(deleteAccountPassword);
-      // GET REQUEST IS INPUT PASSWORD IS CORRECT if request then setDeleteAccountPassword(true)
-      setDeleteAccountPassword(deleteAccountPassword);
+      // Send the inputted password to the backend to check if it is correct
+      setDeleteAccountPasswordCorrect(deleteAccountPassword === "1234");
     }, 500);
 
     return () => clearTimeout(delayDebounceFn);
@@ -32,9 +44,40 @@ export default function InfoAO(props) {
     setChangePassword(!changePassword);
   }
 
+  function handleChangePasswordConfirm() {
+    event.preventDefault();
+    //POST REQUEST TO CHANGE USER PASSWORD IN DATABASE USING AXIOS
+  }
+
   function handleDeleteAccount() {
     props.toggleModal();
     setDeleteAccount(!deleteAccount);
+  }
+
+  function handleDeleteAccountConfirmation() {
+    event.preventDefault();
+    console.log("ola");
+    //DELETE REQUEST TO DELETE USER ACCOUNT IN DATABASE USING AXIOS
+  }
+
+  function handleChangeDataAccount(event) {
+    const { name, value } = event.target;
+    setFormDataAccount((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleChangeDataPassword(event) {
+    const { name, value } = event.target;
+    setFormDataPassword((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
   }
 
   return (
@@ -43,7 +86,43 @@ export default function InfoAO(props) {
         <div className='containerInfoAOTitle'>Account Information</div>
         <hr className='containerInfoAOHr' />
         <div className='containerInfoAOFields'>
-          <InfoAOFields editMode={editMode} />
+          <form action='' className='containerInfoAOFieldsForm'>
+            <label htmlFor='name'>Your Name:</label>
+            <input
+              type='text'
+              readOnly={!editMode}
+              value={formDataAccount.name}
+              onChange={handleChangeDataAccount}
+            />
+            <label htmlFor='name'>Your Email:</label>
+            <input
+              type='text'
+              readOnly={!editMode}
+              value={formDataAccount.email}
+              onChange={handleChangeDataAccount}
+            />
+            <label htmlFor='name'>Your Phone:</label>
+            <input
+              type='text'
+              readOnly={!editMode}
+              value={formDataAccount.phone}
+              onChange={handleChangeDataAccount}
+            />
+            <label htmlFor='name'>Your Fiscal Identifier:</label>
+            <input
+              type='text'
+              readOnly={!editMode}
+              value={formDataAccount.fiscal_identifier}
+              onChange={handleChangeDataAccount}
+            />
+            <label htmlFor='name'>Your Address:</label>
+            <input
+              type='text'
+              readOnly={!editMode}
+              value={formDataAccount.address}
+              onChange={handleChangeDataAccount}
+            />
+          </form>
         </div>
         <div className='containerInfoAOButtons'>
           <button className='containerInfoAOEdit' onClick={handleEdit}>
@@ -71,17 +150,37 @@ export default function InfoAO(props) {
           <hr className='containerInfoAOChangePasswordModalHr' />
           <div className='containerInfoAOChangePasswordModalFields'>
             <form action='' className='containerInfoAOChangePasswordForm'>
-              <label htmlFor='currentPassword'></label>
-              <input type='text' id='currentPassword' name='currentPassword' />
-              <label htmlFor='newPassword'></label>
-              <input type='text' id='newPassword' name='newPassword' />
+              <label htmlFor='currentPassword'>Current Password:</label>
+              <input
+                type='text'
+                id='currentPassword'
+                name='currentPassword'
+                value={formDataPassword.currentPassword}
+                onChange={handleChangeDataPassword}
+              />
+              <label htmlFor='newPassword'>New Password:</label>
+              <input
+                type='text'
+                id='newPassword'
+                name='newPassword'
+                value={formDataPassword.newPassword}
+                onChange={handleChangeDataPassword}
+              />
+              <div className='containerInfoAOModalFormButtons'>
+                <button
+                  className='confirmChangePassword'
+                  onClick={handleChangePasswordConfirm}
+                >
+                  CONFIRM
+                </button>
+                <button
+                  className='cancelChangePassword'
+                  onClick={handleChangePassword}
+                >
+                  CANCEL
+                </button>
+              </div>
             </form>
-            <button
-              className='cancelDeleteAccount'
-              onClick={handleChangePassword}
-            >
-              CANCEL
-            </button>
           </div>
         </div>
       )}
@@ -93,19 +192,37 @@ export default function InfoAO(props) {
           <hr className='containerInfoAODeleteAccountModalHr' />
           <div className='containerInfoAODeleteAccountModalFields'>
             <form action='' className='containerInfoAODeleteAccountForm'>
+              <p className='containerInfoAODeleteAccountFormParagraph'>
+                Are you sure you want to delete your account? This action cannot
+                be undone. In order to delete your account, please type your
+                password below.
+              </p>
               <label htmlFor='currentPassword'></label>
-              <input type='text' id='currentPassword' name='currentPassword' />
-              <button
-                className='confirmDeleteAccount'
-                active={deleteAccountPassword}
-              ></button>
+              <input
+                type='text'
+                id='currentPassword'
+                name='currentPassword'
+                value={deleteAccountPassword}
+                onChange={(event) =>
+                  setDeleteAccountPassword(event.target.value)
+                }
+              />
+              <div className='containerInfoAOModalFormButtons'>
+                <button
+                  className='confirmDeleteAccount'
+                  disabled={!deleteAccountPasswordCorrect}
+                  onClick={handleDeleteAccountConfirmation}
+                >
+                  DELETE
+                </button>
+                <button
+                  className='cancelDeleteAccount'
+                  onClick={handleDeleteAccount}
+                >
+                  CANCEL
+                </button>
+              </div>
             </form>
-            <button
-              className='cancelDeleteAccount'
-              onClick={handleDeleteAccount}
-            >
-              CANCEL
-            </button>
           </div>
         </div>
       )}
