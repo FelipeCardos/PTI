c6f13350c40f504f2ef9ed63854ba5987da89e30
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-11 11:24:12 +0100

    Avanço no backend d eforma a mostrar o backend das encomendas

--- Projeto/code/server/src/controllers/Cart/getCartCost.js
+++ Projeto/code/server/src/controllers/Cart/getCartCost.js
@@ -1,17 +1,15 @@
-const {FindCartLinesWithId} = require('../CartLine/findCartLines');
-const {ProductWithId} = require('../Product/findProducts');
-
+const { FindCartLinesWithId } = require("../CartLine/findCartLines");
+const { ProductWithId } = require("../Product/findProducts");
 
 async function GetCartCost(cartId) {
-    let cost = 0;
-    const cartlines =  await CartLinesWithId(cartId)
-    for (const cartLine of cartlines) {
-        const product = await ProductWithId(cartLine.product_id);
-        cost += product.price * cartLine.amount;
-    }
-    console.log(cost);
-    return cost;
-
+  let cost = 0;
+  const cartlines = await FindCartLinesWithId(cartId);
+  for (const cartLine of cartlines) {
+    const product = await ProductWithId(cartLine.product_id);
+    cost += product.price * cartLine.amount;
+  }
+  console.log(cost);
+  return cost;
 }
 
-module.exports = {GetCartCost};
+module.exports = { GetCartCost };
--- Projeto/code/server/src/routes/api/v1/User/Users.get.js
+++ Projeto/code/server/src/routes/api/v1/User/Users.get.js
@@ -3,35 +3,39 @@ const express = require("express");
 const Address = require("./Address/Users.Address.get");
 const ProductionUnit = require("./ProductionUnit/Users.ProductionUnits.get");
 const Cart = require("./Cart/Users.Carts.get");
+const CartCartLines = require("./Cart/CartLine/Users.Carts.CartLines.get");
 
 const router = express.Router();
 
-const {FindUserById, FindAllUsers} = require('../../../../controllers/User/findUsers');
+const {
+  FindUserById,
+  FindAllUsers,
+} = require("../../../../controllers/User/findUsers");
 
 router.get("/", (req, res) => {
-    FindAllUsers().then((users) => {
-        if (users === null) {
-            res.status(404).send("Not Found");
-        } else {
-            res.status(200).json({users: users});
-        }
-    });
+  FindAllUsers().then((users) => {
+    if (users === null) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).json({ users: users });
+    }
+  });
 });
 
 router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    FindUserById(id).then((user) => {
-        if (user === null) {
-            res.status(404).send("Not Found");
-        } else {
-            res.status(200).json({user: user});
-        }
-    });
+  const id = req.params.id;
+  FindUserById(id).then((user) => {
+    if (user === null) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).json({ user: user });
+    }
+  });
 });
 
 router.use("/:id/address", Address);
 router.use("/:id/productionUnits", ProductionUnit);
 router.use("/:id/carts", Cart);
-
+router.use("/:id/carts/:id/cartLines", CartCartLines);
 
 module.exports = router;
