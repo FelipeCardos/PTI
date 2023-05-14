5ba394bcc0e44ec23d0ede36bbeecd96d16f9758
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-15 00:45:16 +0100

    Temos um json com algumas categorias e subcategorias e alguns atributos

--- Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProductsPMA/AddProducts/AddProducts.jsx
+++ Projeto/code/client/shop-vite/src/components/ProducerManagementArea/ProductsPMA/AddProducts/AddProducts.jsx
@@ -1,17 +1,39 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import categoryData from "../../../../assets/categories.json";
 import "./AddProducts.css";
 
 export default function AddProducts(props) {
   const [formData, setFormData] = useState({
     name: "",
     description: "",
-    productImage: null,
+    productImage: [],
     category: "",
     attributes: [],
     productionDate: "",
     price: "",
   });
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSubcategory, setSelectedSubcategory] = useState("");
+  const [attributes, setAttributes] = useState([]);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+    setSelectedSubcategory("");
+    setAttributes([]);
+  };
+
+  const handleSubcategoryChange = (event) => {
+    setSelectedSubcategory(event.target.value);
+    setAttributes(
+      categoryData.categories
+        .find((category) => category.name === selectedCategory)
+        .subcategories.find(
+          (subcategory) => subcategory.name === event.target.value
+        ).attributes
+    );
+  };
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -19,7 +41,7 @@ export default function AddProducts(props) {
       if (name === "productImage") {
         return {
           ...prevFormData,
-          [name]: event.target.files[0],
+          [name]: event.target.files,
         };
       }
       if (name === "attributes") {
@@ -76,20 +98,66 @@ export default function AddProducts(props) {
           />
         </div>
         <div>
+          <div>
+            <label htmlFor='category'>Category:</label>
+            <select
+              id='category'
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+            >
+              <option value=''>-- Please select a category --</option>
+              {categoryData.categories.map((category) => (
+                <option key={category.name} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+
+            {selectedCategory && (
+              <div>
+                <label htmlFor='subcategory'>Subcategory:</label>
+                <select
+                  id='subcategory'
+                  value={selectedSubcategory}
+                  onChange={handleSubcategoryChange}
+                >
+                  <option value=''>-- Please select a subcategory --</option>
+                  {categoryData.categories
+                    .find((category) => category.name === selectedCategory)
+                    .subcategories.map((subcategory) => (
+                      <option key={subcategory.name} value={subcategory.name}>
+                        {subcategory.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <h3>Attributes:</h3>
+          <ul>
+            {attributes.map((attribute) => (
+              <li key={attribute.name}>
+                <label htmlFor={attribute.name}>{attribute.name}:</label>
+                {attribute.type === "text" && (
+                  <input type='text' id={attribute.name} />
+                )}
+                {attribute.type === "number" && (
+                  <input type='number' id={attribute.name} />
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Gerar os atributos que o fornecedor pode escolher para o seu produto após a escolha da categoria */}
+        <div>
           <input
             type='file'
             onChange={handleChange}
             name='productImage'
-            value={formData.productImage}
+            multiple={true}
           />
-        </div>
-        <div>
-          <input />
-          {/* Fazer um dropdown (das categorias existentes) onde o fornecedor escolhe a categoria que mais se adequa ao produto que ele quer inserir */}
-        </div>
-        <div>
-          <input />
-          {/* Gerar os atributos que o fornecedor pode escolher para o seu produto após a escolha da categoria */}
         </div>
         <div className='buttonsAddProduct'>
           <button
