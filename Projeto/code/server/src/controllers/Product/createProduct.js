12afd83feda87985d61434634f064e45157e1bcc
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-15 01:40:40 +0100

    É preciso uma alteração na base de dados - está especificado no código

--- Projeto/code/server/src/controllers/Product/createProduct.js
+++ Projeto/code/server/src/controllers/Product/createProduct.js
@@ -7,6 +7,7 @@ const { Product } = require("../../database/models");
 //     productionDate: "",
 //     productImages: [],
 //     price: "",
+//     FALTA FAZER UMA ALTERAÇÃO NA BASE DE DADOS - Tabela ProductAttribute guardar o conteudo em vez da CategoryAttribute, ou seja, a CategoryAttribute vai guardar apenas o header como por exemplo "Cor" e o ProductAttribute vai guardar o conteudo como por exemplo "Azul" em conjunto com o product_id e o attribute_id
 
 async function createProduct(
   name,
@@ -23,7 +24,18 @@ async function createProduct(
     price: price,
     productionDate: productionDate,
   });
-  for (let i = 0; i < category.length; i++) {}
+  for (let i = 0; i < category.length; i++) {
+    const productCategory = await ProductCategory.create({
+      product_id: product.id,
+      category_id: category[i],
+    });
+  }
+  for (let i = 0; i < attributes.length; i++) {
+    const productAttribute = await ProductAttribute.create({
+      product_id: product.id,
+      attribute_id: attributes[i],
+    });
+  }
 }
 
 module.exports = { createProduct };
