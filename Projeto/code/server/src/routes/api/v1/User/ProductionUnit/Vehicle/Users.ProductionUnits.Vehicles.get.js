34f29b2b67bace2dd27603a9e8820883a1d59719
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-15 11:56:21 +0100

    Bug corrigido faltava o parametro mergeParams

--- Projeto/code/server/src/routes/api/v1/User/ProductionUnit/Vehicle/Users.ProductionUnits.Vehicles.get.js
+++ Projeto/code/server/src/routes/api/v1/User/ProductionUnit/Vehicle/Users.ProductionUnits.Vehicles.get.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const router = express.Router({ mergeParams: true });
 
-const { FindAllVehiclesFromProductionUnit } = require('../../../../../../controllers/Vehicle/findVehicle');
+const {
+  FindAllVehiclesFromProductionUnit,
+} = require("../../../../../../controllers/Vehicle/findVehicle");
 
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    const id = req.params.productionUnitsId;
-    FindAllVehiclesFromProductionUnit(id).then((vehicles) => {
-        if (vehicles === null) {
-            res.status(404).send("Not Found");
-        } else {
-            res.status(200).json({ vehicles: vehicles });
-        }
-    });
+router.get("/", async (req, res) => {
+  const id = req.params.productionUnitsId;
+  FindAllVehiclesFromProductionUnit(id).then((vehicles) => {
+    if (vehicles === null) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).json({ vehicles: vehicles });
+    }
+  });
 });
 
 module.exports = router;
