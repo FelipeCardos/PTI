a5d7f34a8de04c813e54e5cd4225b56e476199cc
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-24 12:21:39 +0100

    Update Cart para um update mais geral

--- Projeto/code/server/src/controllers/Cart/updateCart.js
+++ Projeto/code/server/src/controllers/Cart/updateCart.js
@@ -1,5 +1,29 @@
 const { Cart } = require("../../database/models");
 
+/**
+ * Updates a cart with all the field given.
+ *
+ * @param id - The id of the cart, we already suppose it exists.
+ * @param consumerId - The id of the owner of the cart.
+ * @param orderDate - The date of the order.
+ * @param deliveryDate - The date of the delivery.
+ * @param status - The status of the cart.
+ * @returns Cart atualizado
+ */
+async function UpdateCart(id, consumerId, orderDate, deliveryDate, status) {
+  const cart = await Cart.findOne({
+    where: {
+      id: id,
+    },
+  });
+  if (consumerId) cart.consumer_id = consumerId;
+  if (orderDate) cart.order_date = orderDate;
+  if (deliveryDate) cart.delivery_date = deliveryDate;
+  if (status) cart.status = status;
+  await cart.save();
+  return cart;
+}
+
 async function UpdateCartOrderDateWithId(cartId, orderDate) {
   const cart = await Cart.findOne({
     where: {
@@ -34,6 +58,7 @@ async function UpdateCartStatusWithId(cartId, status) {
 }
 
 module.exports = {
+  UpdateCart,
   UpdateCartOrderDateWithId,
   UpdateCartDeliveryDateWithId,
   UpdateCartStatusWithId,
