676435914db7248533104d6d022485ec50057519
joaoglmartins <joaoglmartins@noreply.github.com>
2023-05-24 12:36:37 +0100

    Update ConsumerVote

---
+++ Projeto/code/server/src/controllers/ConsumerVote/updateConsumerVote.js
@@ -0,0 +1,19 @@
+const { ConsumerVote } = require("../../database/models");
+
+async function UpdateConsumerVoteUpvoteWithConsumerIdAndCommentId(
+  consumerId,
+  commentId,
+  upvote
+) {
+  const consumerVote = await ConsumerVote.update({
+    where: {
+      consumer_id: consumerId,
+      comment_id: commentId,
+    },
+  });
+  consumerVote.upvote = upvote;
+  await consumerVote.save();
+  return consumerVote;
+}
+
+module.exports = { UpdateConsumerVoteUpvoteWithConsumerIdAndCommentId };
